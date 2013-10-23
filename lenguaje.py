ea3b04fe18c337c7727464772161a03c7f443288
Alejandro <eze.giorgi@gmail.com>
2013-10-22 23:35:20 -0300

    Se hace print de las reglas por las que entra PLY

--- lenguaje.py
+++ lenguaje.py
@@ -47,6 +47,7 @@ def p_programa(p):
     'programa : bloque_dec main'
     p[0] = get_nro_regla()
     crea_terceto(p[1], p[2])
+    print 'programa : bloque_dec main - REGLA 1'
     # Traduccion a assembler aca
 
 def p_bloque_dec(p):
@@ -56,6 +57,11 @@ def p_bloque_dec(p):
         'CIERRA_BLOQUE '
      'PR_ENDEC FIN_LINEA ')
     p[0] = p[4]
+    print ('bloque_dec : PR_DEC DOS_PUNTOS '
+        'ABRE_BLOQUE '
+            'declaraciones '
+        'CIERRA_BLOQUE '
+     'PR_ENDEC FIN_LINEA - REGLA 2')
 
 def p_declaraciones(p):
     'declaraciones : declaracion FIN_LINEA declaraciones'
@@ -63,10 +69,12 @@ def p_declaraciones(p):
     # aca actualizar la tabla de simbolos
     # No habria que crear tercetos
     crea_terceto(p[1], p[3])
+    print 'declaraciones : declaracion FIN_LINEA declaraciones - REGLA 3'
 
 def p_declaraciones_simple(p):
     'declaraciones : declaracion'
     p[0] = p[1]
+    print 'declaraciones : declaracion - REGLA 3'
 
 def p_declaracion(p):
     """
@@ -74,6 +82,7 @@ def p_declaracion(p):
     """
     p[0] = get_nro_regla()
     crea_terceto(p[1], p[3])
+    print 'declaracion : tipo_dato DOS_PUNTOS lista_ids - REGLA 4'
 
 def p_lista_ids(p):
     """
@@ -81,12 +90,14 @@ def p_lista_ids(p):
     """
     p[0] = get_nro_regla()
     crea_terceto(p[1], p[3])
+    print 'lista_ids : ID COMA lista_ids - REGLA 5'
 
 def p_lista_ids_simple(p):
     """
     lista_ids : ID
     """
     p[0] = p[1]
+    print 'lista_ids : ID - REGLA 5'
 
 def p_main(p):
     """
@@ -95,6 +106,7 @@ def p_main(p):
     """
     p[0] = get_nro_regla()
     crea_terceto(p[1], p[2])
+    print 'main : main bloque | main funcion - REGLA 6'
 
 def p_main_simple(p):
     """
@@ -102,6 +114,8 @@ def p_main_simple(p):
     main : funcion
     """
     p[0] = p[1]
+    
+    print 'main : bloque | funcion - REGLA 7'
 
 def p_funcion(p):
     ('funcion : '
@@ -114,6 +128,13 @@ def p_funcion(p):
      )
     p[0] = get_nro_regla()
     crea_terceto(p[2], p[4], p[6])
+    print ('funcion : '
+        'PR_DEF ID DOS_PUNTOS tipo_dato '
+            'ABRE_BLOQUE '
+                'bloque_dec '
+                'bloque '
+            'CIERRA_BLOQUE '
+        'PR_RETURN expresion FIN_LINEA - REGLA 8')
 
 def p_tipo_dato(p):
     """
@@ -122,6 +143,7 @@ def p_tipo_dato(p):
     tipo_dato : PR_STRING
     """
     p[0] = p[1]
+    print 'tipo_dato : PR_INT | PR_FLOAT | PR_STRING - REGLA 9'
 
 def p_bloque(p):
     """
@@ -129,12 +151,14 @@ def p_bloque(p):
     """
     p[0] = get_nro_regla()
     crea_terceto(p[1], p[2])
+    print 'bloque : sentencia bloque - REGLA 10'
 
 def p_bloque_simple(p):
     """
     bloque : sentencia
     """
     p[0] = p[1]
+    print 'bloque : sentencia - REGLA 10'
 
 def p_sentencia(p):
     """
@@ -145,7 +169,10 @@ def p_sentencia(p):
     sentencia : sentencia_percent
     """
     p[0] = p[1]
-    
+    print
+    """
+    sentencia : asig | sentencia_condicional | sentencia_while | sentencia_print | sentencia_percent | - REGLA 11
+    """
 
 def p_sentencia_sentencia(p):
     """
@@ -153,13 +180,14 @@ def p_sentencia_sentencia(p):
     """
     p[0] = get_nro_regla()
     crea_terceto(p[1], p[3])
+    print 'sentencia : sentencia FIN_LINEA sentencia - REGLA 11'
 
 def p_sentencia_print(p):
     """
     sentencia_print : PR_PRINT ID
     """
     p[0] = get_nro_regla()
-    print "Regla nro: " + p[0] + "PR_PRINT ID"  
+    print "sentencia_print : PR_PRINT ID - REGLA 12"  
     
 
 def p_sentencia_while(p):
@@ -169,13 +197,14 @@ def p_sentencia_while(p):
     p[0] = get_nro_regla()
     # (while, condicion, bloque)
     crea_terceto(p[1], p[2], p[5])
-
+    print 'sentencia_while : PR_WHILE condicion DOS_PUNTOS ABRE_BLOQUE bloque CIERRA_BLOQUE - REGLA 13'
 
 def p_sentencia_condicional(p):
     'sentencia_condicional : PR_IF condicion DOS_PUNTOS ABRE_BLOQUE bloque CIERRA_BLOQUE'
     p[0] = get_nro_regla()
     # (if, condicion, bloque)
     crea_terceto(p[1], p[2], p[5])
+    print 'sentencia_condicional : PR_IF condicion DOS_PUNTOS ABRE_BLOQUE bloque CIERRA_BLOQUE - REGLA 14'
     
 def p_sentencia_percent(p):
     """
@@ -184,6 +213,7 @@ def p_sentencia_percent(p):
     p[0] = get_nro_regla()
     # ej: (percent, expresion, expresion)
     crea_terceto(p[1], p[2], p[4])
+    print 'sentencia_percent : PR_PERCENT factor COMA factor - REGLA 15'
 
 def p_asig(p):
     """
@@ -194,6 +224,7 @@ def p_asig(p):
     p[0] = get_nro_regla()
     # (=, ID, exp)
     crea_terceto(p[2], p[1], p[3])
+    print ' asig : ID OP_AS expresion |  ID OP_AS asig | ID OP_AS CTE_STRING - REGLA 16'
 
 def p_condicion(p):
     """
@@ -207,6 +238,14 @@ def p_condicion(p):
     p[0] = get_nro_regla()
     # ej: (<, expresion, expresion)
     crea_terceto(p[2], p[1], p[3])
+    print  """
+    condicion : expresion OP_MAYOR expresion | 
+                expresion OP_MENOR expresion | 
+                expresion OP_MENORIGUAL expresion | 
+                expresion OP_DISTINTO expresion |
+                condicion PR_AND condicion |
+                condicion PR_OR condicion - REGLA 17
+            """
 
 def p_condicion_between(p):
     """
@@ -215,7 +254,8 @@ def p_condicion_between(p):
     p[0] = get_nro_regla()
     # ej: (<, expresion, expresion)
     crea_terceto(p[2], p[1], p[3], p[5])
-
+    print  'condicion : factor PR_BETWEEN factor PR_AND factor - REGLA 17'
+    
 def p_expresion(p):
     """
     expresion : expresion OP_SUMA termino
@@ -226,20 +266,24 @@ def p_expresion(p):
     p[0] = get_nro_regla()
     # (+, exp, ter)
     crea_terceto(p[2], p[1], p[3])
-
+    print 'expresion: expresion + termino | expresion - termino | expresion * termino | expresion / termino - REGLA 18'
+    
 def p_expression_term(p):
     'expresion : termino'
     p[0] = p[1]
-
+    print 'expresion : termino - REGLA 18'
+    
 def p_term_factor(p):
     'termino : factor'
     p[0] = p[1]
+    print 'termino : factor - REGLA 19'
 
 def p_factor_parentesis(p):
     """
     factor : PAREN_ABRE expresion PAREN_CIERRA
     """
     p[0] = p[2]
+    print 'factor : ( expresion ) - REGLA 20'
 
 def p_factor(p):
     """
@@ -248,6 +292,7 @@ def p_factor(p):
     factor : ID
     """
     p[0] = p[1]
+    print 'factor : CTE_ENT | CTE_REAL | ID - REGLA 21'
 
 # Error rule for syntax errors
 def p_error(p):
@@ -264,14 +309,14 @@ except:
     filename = "fuente.zz"
 fuente = open(filename).read()
 
-print "Tokens: "
+#print "Tokens: "
 result = yyparse.parse(fuente, lexer=yylex)
-if result is not None:
-    print "\nTercetos:\n"
-    for i, terceto in enumerate(tercetos):
-        print "%s: %s" % (i, terceto)
-else:
-    print "\nError!!"
+#if result is not None:
+#    print "\nTercetos:\n"
+#    for i, terceto in enumerate(tercetos):
+#        print "%s: %s" % (i, terceto)
+#else:
+#    print "\nError!!"
 
 # while True:
     # s = "2+3"
